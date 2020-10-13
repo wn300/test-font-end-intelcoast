281b837209279747be61706e8384cb8832dbd8fc
Wilmer David Mancera <wilmer@iMac-de-Apple-3.local>
2020-10-12 23:43:33 -0500

    deploy hg pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled',
+    useHash: true,
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
